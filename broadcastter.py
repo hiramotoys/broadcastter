1dfef4d9a1ffad9656d41993244bfe44d445bf88
hiramotoys <hiramoto.ys@gmail.com>
2011-02-11 03:17:05 +0900

    remnamed

--- broadcastter.py
+++ broadcastter.py
@@ -6,4 +6,4 @@ appdetail = yaml.load(str)
 auth = tweepy.OAuthHandler(appdetail['consumer_key'], appdetail['consumer_secret'])
 auth.set_access_token(appdetail['access_key'], appdetail['access_secret'])  
 api = tweepy.API(auth)
-
+api.update_status("test")
