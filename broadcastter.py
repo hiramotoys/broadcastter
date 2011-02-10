f4ac3592d28b7a3551c3d32382e0a7201f4f415a
hiramotoys <hiramoto.ys@gmail.com>
2011-02-11 03:28:51 +0900

    csvから送信先のスクリーンネームを読み込むようにした

--- broadcastter.py
+++ broadcastter.py
@@ -1,9 +1,17 @@
 import tweepy
 import yaml
+import csv
+import time
 
 str = open('appdetail.yaml').read()
 appdetail = yaml.load(str)
 auth = tweepy.OAuthHandler(appdetail['consumer_key'], appdetail['consumer_secret'])
 auth.set_access_token(appdetail['access_key'], appdetail['access_secret'])  
 api = tweepy.API(auth)
-api.update_status("test")
+filename = 'namelist.csv'
+csvfile = open(filename)
+for row in csv.reader(csvfile):
+    for screen_name in row:
+        api.update_status("@" + screen_name + " " + "hello")
+        time.sleep(10)
+print 'sent mentions'
